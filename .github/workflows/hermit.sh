42f396eadd6249773d6e12c22c0ec6196864ae72
James Strachan <james.strachan@gmail.com>
2021-07-28 12:17:28 +0100

    fix: add hermit jx upgrades

---
+++ .github/workflows/hermit.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+echo "upgrading the hermit repositories"
+
+if [ -z "$GITHUB_ACTIONS" ]
+then
+  echo "not setting up git as not in a GitHub Action"
+else
+  echo "lets setup git"
+  git config user.name github-actions
+  git config user.email github-actions@github.com
+fi
+
+declare -a repos=(
+  "jx3-kind"
+)
+
+export TMPDIR=/tmp/jx3-gitops-promote
+rm -rf $TMPDIR
+mkdir -p $TMPDIR
+
+for r in "${repos[@]}"
+do
+  echo "upgrading repository https://github.com/jx3-gitops-repositories/$r"
+  cd $TMPDIR
+  git clone https://github.com/jx3-gitops-repositories/$r.git
+  cd "$r"
+
+  echo "synchronising hermit"
+
+  ./bin/hermit env --raw >> $GITHUB_ENV
+  hermit sync
+
+  echo "upgrading the jx version"
+  hermit upgrade jx
+
+  git add * || true
+  git commit -a -m "chore: upgrade jx version" || true
+  git push || true
+done
+
+
+
+
